1c6c1d1edc9f7356b4071a5b1155dcb88d03f05c
3gypt <104285004+3gypt@users.noreply.github.com>
2022-04-23 21:36:49 -0400

    Add files via upload

---
+++ sipmle madlibs.py
@@ -0,0 +1,7 @@
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+noun = input("Emter a noun: ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I hate " + noun)
