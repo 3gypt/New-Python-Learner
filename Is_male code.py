bb9d160e64426979b5bcda31cd6acf04af2cec95
3gypt <104285004+3gypt@users.noreply.github.com>
2022-04-25 15:23:00 -0400

    Add files via upload

---
+++ Is_male code.py
@@ -0,0 +1,22 @@
+is_male = False
+is_tall = False
+
+if is_male and is_tall:
+    print("Hello, you are a male, and tall.")
+elif is_male and not (is_tall):
+    print("You are a short male")
+
+elif not is_male and (is_tall):
+    print("You are not a male but tall")
+else:
+    print("Hello, you are not a male, and not tall.")
+
+    
+
+
+
+
+
+
+
+
