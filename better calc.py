0dda0b1d7b9b2bd2a36bfa4a94dc2a78daf6ad4d
3gypt <104285004+3gypt@users.noreply.github.com>
2022-04-25 15:27:05 -0400

    Update better calc.py

--- better calc.py
+++ better calc.py
@@ -4,7 +4,7 @@ op = (input("Enter operator "))
 num2 = float(input("Enter second number: "))
 
 if op == "+":
-    print(num1 = num2)
+    print(num1 + num2)
 elif op == "-":
     print(num1 - num2)
 elif op == "/":
